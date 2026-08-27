928bde04612ce625e4cd96cc25111fccabeaec2a
jamalalmo7 <jamalalmo74@gmail.com>
2026-08-27 19:21:18 +0300

    Add PaymentManager class

---
+++ src/vehicle_management_rental_system/PaymentManager.java
@@ -0,0 +1,14 @@
+
+
+package vehicle_management_rental_system;
+
+import java.util.ArrayList;
+
+
+public class PaymentManager {
+    private ArrayList<Payment> paymentList = new ArrayList<>();
+    Rental rental;
+
+
+    
+}
